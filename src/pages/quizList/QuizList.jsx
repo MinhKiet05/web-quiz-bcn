41dfc26ba5f75befddf6dd1a5b5e0d5e73930833
MinhKiet05 <tranvanbetu1965@gmail.com>
2026-09-05 22:52:39 +0700

    fix ui cho card

--- src/pages/quizList/QuizList.jsx
+++ src/pages/quizList/QuizList.jsx
@@ -58,7 +58,7 @@ export default function QuizList() {
         }
       }
 
-      const categoryMap = { 1: 'C/C++', 2: 'Mobile (Java)', 3: 'Web (HTML/CSS/JavaScript)' };
+      const categoryMap = { 1: 'C/C++', 2: 'Mobile (Java)', 3: 'Web' };
 
       // 4. Chuẩn hóa dữ liệu & Gắn cờ is_completed
       let formattedQuizzes = quizData.map((q) => {
